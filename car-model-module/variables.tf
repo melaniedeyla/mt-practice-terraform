1bba61f7200f0efa423ab5230b51c62f00789e57
Melanie Deyla <melaniedeyla@Melanies-MacBook-Air.local>
2023-09-11 22:26:39 +1000

    PR-1 add files for first module

---
+++ car-model-module/variables.tf
@@ -0,0 +1,31 @@
+variable "model_name" {
+  description = "Name of the car model"
+}
+
+variable "make" {
+  description = "Make of the car model"
+}
+
+variable "year" {
+  description = "Year of the car model"
+}
+
+variable "price" {
+  description = "Price of the car model"
+}
+
+variable "color" {
+  description = "Color of the car model"
+}
+
+variable "fuel_type" {
+  description = "Fuel type of the car model"
+}
+
+variable "horsepower" {
+  description = "Horsepower of the car model"
+}
+
+variable "transmission" {
+  description = "Transmission type of the car model"
+}
